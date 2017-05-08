7bf863907d05a144e8e6e4027beb34f4233cbe6c
Matheus Macedo Lopes <matheusmlopess@gmail.com>
2017-05-08 18:51:54 -0300

    github_ssh added

---
+++ github_ssh.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+	echo ${yel}
+	local SSH_CHK=$(eval "git remote -v");
+	echo $(pwd)
+	echo ${end}
+	printf "\n"
+
+		if [[ $SSH_CHK == *"git@github.com:matheusmlopess"* ]]; then
+	  		echo "ssh checked and all good! ...:"
+			git remote -v;
+		elif [[ $SSH_CHK == *"https://github.com/matheusmlopess"* ]];then
+			
+			repository=${PWD##*/}
+ 			read -p "is ('$repository' ) the right github repository [Y/n]" -r YesNo; 
+				case "$YesNo" in
+					[yY][eE][sS]|[yY]) 
+					
+					git remote set-url origin git@github.com:matheusmlopess/$repository.git
+			                echo "All good now... ssh set and running"
+					git remote -v;
+					;;
+					*)
+				       	read -p " Respository name (clone path) :" -r respositoryname 
+					git remote set-url origin git@github.com:matheusmlopess/$repositoryname.git
+					echo "All good now... ssh set and running"
+					git remote -v 
+					;;
+				esac
+		elif [[ $1 == "0" ]]; then
+			cd $2
+			echo Applying ssh new repository permissions ... son!
+			git remote set-url origin git@github.com:matheusmlopess/$2.git
+
+			echo ${cyn} ssh permission successfully done: 
+			git remote -v 
+			echo ${end}
+
+		else	
+			git remote -v;
+		fi
+
+
