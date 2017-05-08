ce91d477354179773180c6e11a74944a97e2ed51
Matheus Macedo Lopes <matheusmlopess@gmail.com>
2017-05-08 19:00:37 -0300

    pkg_status add

---
+++ pkg_status.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+		local qualPacote=$1
+	      #----------------------
+			local XPKG_CHK=0;   #flag ... 
+			# dont require sudo permissions
+			# 2>&1 :Passing stderr (2) over "-v" pipe along with stdout (1)
+			# by redirecting the stderr stream (file descriptor #2)
+			# to stdout (file descriptor #1)	
+			command -v $qualPacote >/dev/null 2>&1 || { XPKG_CHK=1;}
+			if [ $XPKG_CHK -eq 0 ];
+			then
+				#TOP 
+				apt-cache policy ${qualPacote};
+			else	
+				read -r -p "Pakage not found... Do you want to install it [Y/n]? " response
+				case "$response" in
+					[yY][eE][sS]|[yY]) 
+						sudo apt-get install $qualPacote;
+					;;
+					*)
+						echo okay fine ...;	
+				        ;;
+				esac
+			fi
